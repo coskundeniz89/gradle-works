b05fc821b84f97359ef543b6cbd513b2a4010f76
Coşkun Deniz <coskundeniz1989@gmail.com>
2017-01-31 00:35:14 +0300

    Second app for gradle dependencies

---
+++ 02-gradle-dependency/src/main/java/com/teammental/HelloWorldDependency.java
@@ -0,0 +1,12 @@
+package com.teammental;
+
+/**
+ * Created by Coşkun on 31.1.2017.
+ */
+public class HelloWorldDependency {
+
+    public static void main(String[] args) {
+        System.out.println("Merhaba Türkiye");
+    }
+
+}
