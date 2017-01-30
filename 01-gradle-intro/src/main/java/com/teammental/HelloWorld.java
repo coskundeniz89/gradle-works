6b92642aa7f9f7bd45690e6b8b4588b138fc4178
Coşkun Deniz <coskundeniz1989@gmail.com>
2017-01-31 00:30:50 +0300

    First sample main class

---
+++ 01-gradle-intro/src/main/java/com/teammental/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.teammental;
+
+/**
+ * Created by Coşkun on 31.1.2017.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Merhaba Türkiye");
+    }
+}
