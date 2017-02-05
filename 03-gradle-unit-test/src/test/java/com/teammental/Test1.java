08058ab5edecd337b10c8a61a54c89477132b69a
Coşkun Deniz <coskundeniz1989@gmail.com>
2017-02-06 00:13:05 +0300

    Testler eklendi

---
+++ 03-gradle-unit-test/src/test/java/com/teammental/Test1.java
@@ -0,0 +1,15 @@
+package com.teammental;
+
+import org.junit.Test;
+
+/**
+ * Created by Coşkun on 3.2.2017.
+ */
+public class Test1 {
+
+    @Test
+    public void test1() {
+        System.out.println("This a default unit test");
+    }
+
+}
---
+++ 03-gradle-unit-test/src/test/java/com/teammental/UnitTest.java
@@ -0,0 +1,10 @@
+package com.teammental;
+
+/**
+ * This is a marker interface that is used to specify that a test class
+ * belongs to the unit test suite.
+ *
+ * @author Petri Kainulainen
+ */
+interface UnitTest {
+}
